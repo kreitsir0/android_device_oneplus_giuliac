51388f354799e795caebad60ac49b7552a849b1f
Kreitsir <kreitsir@gmail.com>
2026-08-04 11:19:32 +0300

    crdroid-ify

--- device.mk
+++ device.mk
@@ -92,3 +92,7 @@ $(call inherit-product, device/oneplus/sm8650-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/giuliac/giuliac-vendor.mk)
+
+# Remove Packages
+PRODUCT_PACKAGES_REMOVE += \
+    Aperture
--- lineage_giuliac.mk
+++ lineage_giuliac.mk
@@ -14,14 +14,6 @@ $(call inherit-product, device/oneplus/giuliac/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Lunaris Flags
-LUNARIS_BUILD_TYPE := OFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_CUSTOM_UDFPS := true
-TARGET_OPTIMIZED_DEXOPT := true
-TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
-WITH_GMS := true
-WITH_BCR := true
 
 PRODUCT_NAME := lineage_giuliac
 PRODUCT_DEVICE := giuliac
